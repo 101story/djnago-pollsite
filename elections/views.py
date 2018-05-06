ed02471ad6239dcb06ba8ad74409265f5c8500f8
101story <hooraystory@gmail.com>
2018-05-06 15:14:40 +0900

    add candidate model

--- elections/views.py
+++ elections/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Candidate
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello There")
+    candidates = Candidate.objects.all()
+    # str=''
+    # for candidate in candidates:
+    #     str += "{0} 기호{1}번({2}) <br>".format(candidate.name,candidate.party_number,candidate.area)
+    #     str += candidate.introduction+" <p>"
+    # return HttpResponse(str)
+    context = {'candidates':candidates}
+    return render(request, 'elections/index.html', context)
