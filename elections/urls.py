52311d628d22818f99bae6a2eb2af50916c6305d
101story <hooraystory@gmail.com>
2018-05-08 19:32:43 +0900

    static file and layout

--- elections/urls.py
+++ elections/urls.py
@@ -3,9 +3,10 @@ from django.conf.urls import url
 
 from . import views
 
+app_name='elections'
 urlpatterns = [
     # 빈경로인 경우 views.index 함수가 실행되도록 해라
-    path('', views.index, name='index'),
+    path('', views.index, name='home'),
     url(r'^areas/(?P<area>[가-힣]+)/$', views.areas),
     url(r'^areas/(?P<area>[가-힣]+)/results$', views.results),
     url(r'^polls/(?P<poll_id>\d+)/$', views.polls),
